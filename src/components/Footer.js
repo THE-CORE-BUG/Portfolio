51fd691ebe2bca6c4e1dfb3acba9c00f0116bf51
Vikas Kumar Saw <vikasvks0112@gmail.com>
2021-07-29 12:23:37 +0530

    fixed bug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,39 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import {SiGmail} from "react-icons/si";
+import { SiGmail } from "react-icons/si";
 
 function Footer() {
-  const [time,setTime] = useState(new Date());
+  const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    const timer = setInterval(setTime(new Date()),1000);
-    return () => clearInterval(timer)
-  })
+    const timer = setInterval(setTime(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, [time]);
 
   return (
     <Container fluid className="footer">
       <Row>
-
-
         <Col md="4" className="footer-copywright">
-          <h3>Made with 🌶️ by <span style={{color: '#fff'}}><b>Ritam Kabiraj</b></span></h3>
+          <h3>
+            Made with 🌶️ by{" "}
+            <span style={{ color: "#fff" }}>
+              <b>Ritam Kabiraj</b>
+            </span>
+          </h3>
         </Col>
-
 
         <Col md="4" className="footer-copywright">
           <h3>{time.toLocaleTimeString()} &nbsp; IST</h3>
         </Col>
 
-
         <Col md="4" className="footer-body">
-          <p style={{color: "white"}} >Drop a Mail - &nbsp; <a href="mailto:ritamkaviraj26@gmail.com "> <SiGmail/> </a> </p>
+          <p style={{ color: "white" }}>
+            Drop a Mail - &nbsp;{" "}
+            <a href="mailto:ritamkaviraj26@gmail.com ">
+              {" "}
+              <SiGmail />{" "}
+            </a>{" "}
+          </p>
         </Col>
       </Row>
     </Container>
